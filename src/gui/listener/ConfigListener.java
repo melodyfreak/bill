2730c1965e6e754cfbe3c3d6c363185c1b87016e
HP <HP@LAPTOP-NKNFD873>
2022-04-21 16:34:04 +0800

    test

---
+++ src/gui/listener/ConfigListener.java
@@ -0,0 +1,37 @@
+package gui.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JOptionPane;
+
+import gui.panel.ConfigPanel;
+import service.ConfigService;
+import util.GUIUtil;
+
+public class ConfigListener implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		ConfigPanel p=ConfigPanel.instance;
+		if(!GUIUtil.checkNumber(p.tfBudget, "本月预算"))
+			return;
+		String mysqlPath= p.tfMysqlPath.getText();
+		if(0!=mysqlPath.length()){
+			File commandFile=new File(mysqlPath,"bin/mysql.exe");
+			if(!commandFile.exists()) {
+				JOptionPane.showMessageDialog(p, "路径不对");
+				p.tfMysqlPath.grabFocus();
+				return;
+			}
+		}
+		ConfigService cs=new ConfigService();
+		cs.update(ConfigService.budget, p.tfBudget.getText());
+		cs.update(ConfigService.mysqlPath, mysqlPath);
+		
+		JOptionPane.showMessageDialog(p, "修改成功");
+	}
+	
+
+}
---
+++ src/gui/model/CategoryTableModel.java
@@ -0,0 +1,73 @@
+package gui.model;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+
+import entity.Category;
+import service.CategoryService;
+ 
+public class CategoryTableModel extends AbstractTableModel{
+ 
+    String[] columnNames = new String[]{"分类名称","消费次数"};
+    public List<Category> cs = new CategoryService().list();
+     
+   
+    @Override
+    public int getRowCount() {
+        // TODO Auto-generated method stub
+        return cs.size();
+    }
+ 
+    @Override
+    public int getColumnCount() {
+        // TODO Auto-generated method stub
+        return columnNames.length;
+    }
+ 
+    @Override
+    public String getColumnName(int columnIndex) {
+        // TODO Auto-generated method stub
+        return columnNames[columnIndex];
+    }
+ 
+ 
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+ 
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        // TODO Auto-generated method stub
+    	Category h=cs.get(rowIndex);
+        if(0==columnIndex)
+            return h.name;
+        if(1==columnIndex)
+            return h.recordNumber;
+        return null;
+    }
+ 
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        // TODO Auto-generated method stub
+         
+    }
+ 
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+        // TODO Auto-generated method stub
+         
+    }
+ 
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+        // TODO Auto-generated method stub
+         
+    }
+ 
+}
